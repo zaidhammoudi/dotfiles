01d4ae415e3585c1cbebbaff45c41be55aed8066
Zaid Hammoudi <7748859+zaidhammoudi@users.nore        ply.github.com>
2025-12-17 21:29:14 -0800

    Switch from zgen to zinit and clean up zshrc
      - Replace zgen plugin manager with zinit (zgen config preserved as comment)
      - Remove zgen.zsh and its install script symlink
      - Add explicit HISTFILE setting
      - Fix GOROOT typo and move to Darwin-only block
      - Add ag command check before setting FZF defaults
      - Remove unused g-install GOPATH/GOROOT config

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,12 +7,3 @@ ZSH_SOURCE="$HOME/code/dotfiles/zsh/zshrc"
 ZSH_TARGET="$HOME/.zshrc"
 ln -sf $ZSH_SOURCE $ZSH_TARGET
 echo "Symlink created: $ZSH_TARGET → $ZSH_SOURCE"
-
-
-ZGEN_SOURCE="$HOME/code/dotfiles/zsh/zgen.zsh"
-
-ZGEN_TARGET="$HOME/.zgen/zgen.zsh"
-mkdir -p "$HOME/.zgen"
-
-ln -sf $ZGEN_SOURCE $ZGEN_TARGET
-echo "Symlink created: $ZGEN_TARGET → $ZGEN_SOURCE"
